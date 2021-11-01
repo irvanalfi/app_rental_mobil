be245972e2268ff09b652fdf0abfbf18dfcc99e1
irvanalfi <44670772+irvanalfi@users.noreply.github.com>
2021-11-01 13:48:07 +0700

    update db rental_mobil

--- rental_mobil.sql
+++ rental_mobil.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 30 Sep 2021 pada 17.20
--- Versi server: 10.4.20-MariaDB
--- Versi PHP: 8.0.8
+-- Generation Time: Nov 01, 2021 at 07:47 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,74 +24,70 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `admin`
---
-
-CREATE TABLE `admin` (
-  `id_admin` int(11) NOT NULL,
-  `nama_admin` varchar(120) NOT NULL,
-  `username` varchar(120) NOT NULL,
-  `password` varchar(120) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `contact`
+-- Table structure for table `contact`
 --
 
 CREATE TABLE `contact` (
   `id_contact` int(11) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `subject` varchar(100) NOT NULL,
   `pesan` text NOT NULL,
   `status` enum('1','0') NOT NULL COMMENT '1=sudah dilihan, 0=belum di lihat',
-  `created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated` datetime DEFAULT NULL
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `customer`
+-- Table structure for table `fitur`
 --
 
-CREATE TABLE `customer` (
-  `id_customer` int(11) NOT NULL,
-  `nama` varchar(120) NOT NULL,
-  `username` varchar(120) NOT NULL,
-  `alamat` varchar(120) NOT NULL,
-  `gender` varchar(20) NOT NULL,
-  `no_telepon` varchar(20) NOT NULL,
-  `no_ktp` varchar(50) NOT NULL,
-  `password` varchar(120) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `fitur` (
+  `id_fitur` int(11) NOT NULL,
+  `id_mobil` int(11) NOT NULL,
+  `hrg_supir` int(11) NOT NULL,
+  `supir` int(1) NOT NULL,
+  `ac` int(1) NOT NULL,
+  `seat_belt` int(1) NOT NULL,
+  `air` int(1) NOT NULL,
+  `p3k` int(1) NOT NULL,
+  `audio_input` int(1) NOT NULL,
+  `mp3_player` int(1) NOT NULL,
+  `bluetooth` int(1) NOT NULL,
+  `vidio` int(1) NOT NULL,
+  `central_lock` int(1) NOT NULL,
+  `ban_serep` int(1) NOT NULL,
+  `car_kit` int(1) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `customer`
+-- Dumping data for table `fitur`
 --
 
-INSERT INTO `customer` (`id_customer`, `nama`, `username`, `alamat`, `gender`, `no_telepon`, `no_ktp`, `password`, `role_id`, `created`, `updated`) VALUES
-(4, 'Joko Santoso', 'joko', 'Jl. Satu Pekanbaru', 'laki-laki', '0653246512', '215654532767', '81dc9bdb52d04dc20036dbd8313ed055', 2, '2021-09-30 12:23:12', NULL),
-(5, 'Darmawan', 'darmawan', 'Jl. Dua Pekanbaru', 'laki-laki', '07617623', '1423477324723', '81dc9bdb52d04dc20036dbd8313ed055', 2, '2021-09-30 12:23:12', NULL),
-(6, 'Andi', 'andi', 'Jakarta', 'laki-laki', '0217687634', '12747657463', '827ccb0eea8a706c4c34a16891f84e7b', 2, '2021-09-30 12:23:12', NULL),
-(7, 'Arif', 'admin', 'Pekanbaru', 'laki-laki', '065423624', '1764578345', '21232f297a57a5a743894a0e4a801fc3', 1, '2021-09-30 12:23:12', NULL),
-(8, 'Bayu', 'bayu', 'Jl. Nangka Pekanbaru', 'laki-laki', '07612233', '14000756764735', '81dc9bdb52d04dc20036dbd8313ed055', 2, '2021-09-30 12:23:12', NULL),
-(9, 'Toni', 'toni', 'Bandung', 'laki-laki', '0835653243', '1753453265435', '81dc9bdb52d04dc20036dbd8313ed055', 2, '2021-09-30 12:23:12', NULL);
+INSERT INTO `fitur` (`id_fitur`, `id_mobil`, `hrg_supir`, `supir`, `ac`, `seat_belt`, `air`, `p3k`, `audio_input`, `mp3_player`, `bluetooth`, `vidio`, `central_lock`, `ban_serep`, `car_kit`, `created`, `updated`, `created_by`, `updated_by`) VALUES
+(1, 6, 300000, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, NULL, NULL, 0, 0),
+(2, 9, 300000, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, NULL, NULL, 0, 0),
+(3, 10, 300000, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, NULL, NULL, 0, 0),
+(4, 13, 300000, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, NULL, NULL, 0, 0),
+(5, 14, 300000, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, NULL, NULL, 0, 0),
+(6, 15, 300000, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, NULL, NULL, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `mobil`
+-- Table structure for table `mobil`
 --
 
 CREATE TABLE `mobil` (
   `id_mobil` int(11) NOT NULL,
-  `kode_tipe` varchar(120) NOT NULL,
+  `id_tipe` int(11) NOT NULL,
   `merek` varchar(120) NOT NULL,
   `no_plat` varchar(20) DEFAULT NULL,
   `warna` varchar(20) DEFAULT NULL,
@@ -104,104 +100,78 @@ CREATE TABLE `mobil` (
   `status` int(11) NOT NULL,
   `harga` int(11) NOT NULL,
   `denda` int(11) NOT NULL,
-  `hrg_supir` int(11) NOT NULL,
-  `sopir` int(11) DEFAULT NULL COMMENT 'status lepas kunci/tidak',
-  `ac` int(11) DEFAULT NULL,
-  `seat_belt` int(11) DEFAULT NULL,
-  `air` int(11) DEFAULT NULL,
-  `p3k` int(11) DEFAULT NULL,
-  `audio_input` int(11) DEFAULT NULL,
-  `mp3_player` int(11) DEFAULT NULL,
-  `bluethooth` int(11) DEFAULT NULL,
-  `vidio` int(11) DEFAULT NULL,
-  `central_lock` int(11) DEFAULT NULL,
-  `ban_serep` int(11) DEFAULT NULL,
-  `car_kit` int(11) DEFAULT NULL,
   `gambar` varchar(255) NOT NULL,
   `detail` text DEFAULT NULL,
-  `created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated` datetime DEFAULT NULL
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `mobil`
+-- Dumping data for table `mobil`
 --
 
-INSERT INTO `mobil` (`id_mobil`, `kode_tipe`, `merek`, `no_plat`, `warna`, `transmisi`, `jmlh_kursi`, `bagasi`, `bbm`, `tahun`, `km`, `status`, `harga`, `denda`, `hrg_supir`, `sopir`, `ac`, `seat_belt`, `air`, `p3k`, `audio_input`, `mp3_player`, `bluethooth`, `vidio`, `central_lock`, `ban_serep`, `car_kit`, `gambar`, `detail`, `created`, `updated`) VALUES
-(6, 'SDN', 'Toyota Camry', 'B 1446 DAG', 'Hitam', 'Manual', '7 Dewasa', '3 Ransel', 'Pertalite', 2015, 21000, 1, 400000, 30000, 300000, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 'toyota-camry.jpg', '', '2021-09-30 12:20:55', NULL),
-(9, 'SDN', 'Honda City', 'B 1456 DAG', 'Hitam', 'Manual', '7 Dewasa', '3 Ransel', 'Pertamax', 2015, 11860, 0, 450000, 30000, 300000, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 'honda-city.jpg', '', '2021-09-30 12:20:55', NULL),
-(10, 'SDN', 'CRV', 'B 1234 csh', 'Silver', 'Matic', '7 Dewasa', '4 Ransel', 'Pertalite', 2019, 34000, 0, 400000, 100000, 300000, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 'gallery_used-car-mobil123-honda-cr-v-2_4.jpg', '', '2021-09-30 12:20:55', NULL),
-(13, 'MNV', 'Toyota Avanza', 'B 2245 DAM', 'Hitam', 'Manual', '7 Dewasa', '4 Ransel', 'Pertamax', 2015, 25640, 1, 350000, 25000, 300000, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 'avanza-hitam.jpg', '', '2021-09-30 12:20:55', NULL),
-(14, 'MNV', 'Toyota Avanza', 'B 1123 DUD', 'Putih', 'Matic', '7 Dewasa', '4 Ransel', 'Pertalite', 2016, 40020, 1, 350000, 25000, 300000, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 'avanza-putih.jpg', '', '2021-09-30 12:20:55', NULL),
-(15, 'ELF', 'Toyota Hiace', 'P 1234 UI', 'Silver', 'Manual', '10 Dewasa', '8 Koper', 'Solar', 2015, 19762, 1, 700000, 100000, 300000, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 'car-300921-b3bd9ca18c.jpg', 'kdfjkls akdjfa skdjf ladkljf klsajfkla klsdfjkla lsdkjflas lkjfskld', '2021-09-30 14:44:05', NULL);
+INSERT INTO `mobil` (`id_mobil`, `id_tipe`, `merek`, `no_plat`, `warna`, `transmisi`, `jmlh_kursi`, `bagasi`, `bbm`, `tahun`, `km`, `status`, `harga`, `denda`, `gambar`, `detail`, `created`, `updated`, `created_by`, `updated_by`) VALUES
+(6, 0, 'Toyota Camry', 'B 1446 DAG', 'Hitam', 'Manual', '7 Dewasa', '3 Ransel', 'Pertalite', 2015, 21000, 1, 400000, 30000, 'toyota-camry.jpg', '', '2021-09-30 19:20:55', NULL, 0, 0),
+(9, 0, 'Honda City', 'B 1456 DAG', 'Hitam', 'Manual', '7 Dewasa', '3 Ransel', 'Pertamax', 2015, 11860, 0, 450000, 30000, 'honda-city.jpg', '', '2021-09-30 19:20:55', NULL, 0, 0),
+(10, 0, 'CRV', 'B 1234 csh', 'Silver', 'Matic', '7 Dewasa', '4 Ransel', 'Pertalite', 2019, 34000, 0, 400000, 100000, 'gallery_used-car-mobil123-honda-cr-v-2_4.jpg', '', '2021-09-30 19:20:55', NULL, 0, 0),
+(13, 0, 'Toyota Avanza', 'B 2245 DAM', 'Hitam', 'Manual', '7 Dewasa', '4 Ransel', 'Pertamax', 2015, 25640, 1, 350000, 25000, 'avanza-hitam.jpg', '', '2021-09-30 19:20:55', NULL, 0, 0),
+(14, 0, 'Toyota Avanza', 'B 1123 DUD', 'Putih', 'Matic', '7 Dewasa', '4 Ransel', 'Pertalite', 2016, 40020, 1, 350000, 25000, 'avanza-putih.jpg', '', '2021-09-30 19:20:55', NULL, 0, 0),
+(15, 0, 'Toyota Hiace', 'P 1234 UI', 'Silver', 'Manual', '10 Dewasa', '8 Koper', 'Solar', 2015, 19762, 1, 700000, 100000, 'car-300921-b3bd9ca18c.jpg', 'kdfjkls akdjfa skdjf ladkljf klsajfkla klsdfjkla lsdkjflas lkjfskld', '2021-09-30 21:44:05', NULL, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `rental`
---
-
-CREATE TABLE `rental` (
-  `id_rental` int(11) NOT NULL,
-  `id_customer` int(11) NOT NULL,
-  `id_mobil` int(11) NOT NULL,
-  `tgl_rental` date NOT NULL,
-  `tgl_kembali` date NOT NULL,
-  `harga` varchar(120) NOT NULL,
-  `denda` varchar(120) NOT NULL,
-  `tgl_pengembalian` date NOT NULL,
-  `status_pengembalian` varchar(50) NOT NULL,
-  `status_rental` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `review`
+-- Table structure for table `review`
 --
 
 CREATE TABLE `review` (
   `id_review` int(11) NOT NULL,
-  `id_customer` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `review` text NOT NULL,
   `star` int(2) NOT NULL,
   `status` enum('1','0') NOT NULL COMMENT '1 = tampil, 0 = tidak tampil',
-  `created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated` datetime DEFAULT NULL
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tipe`
+-- Table structure for table `tipe`
 --
 
 CREATE TABLE `tipe` (
   `id_tipe` int(11) NOT NULL,
   `kode_tipe` varchar(120) NOT NULL,
   `nama_tipe` varchar(120) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated` datetime DEFAULT NULL
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `tipe`
+-- Dumping data for table `tipe`
 --
 
-INSERT INTO `tipe` (`id_tipe`, `kode_tipe`, `nama_tipe`, `created`, `updated`) VALUES
-(1, 'SDN', 'Sedan', '2021-09-30 12:21:47', NULL),
-(3, 'MNV', 'Mini Van', '2021-09-30 12:21:47', NULL),
-(4, 'ELF', 'Elef', '2021-09-30 14:40:39', NULL);
+INSERT INTO `tipe` (`id_tipe`, `kode_tipe`, `nama_tipe`, `created`, `updated`, `created_by`, `updated_by`) VALUES
+(1, 'SDN', 'Sedan', '2021-09-30 19:21:47', NULL, 0, 0),
+(3, 'MNV', 'Mini Van', '2021-09-30 19:21:47', NULL, 0, 0),
+(4, 'ELF', 'Elef', '2021-09-30 21:40:39', NULL, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `transaksi`
+-- Table structure for table `transaksi`
 --
 
 CREATE TABLE `transaksi` (
   `id_rental` int(11) NOT NULL,
-  `id_customer` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `id_mobil` int(11) NOT NULL,
   `tgl_rental` date NOT NULL,
   `tgl_kembali` date NOT NULL,
@@ -212,124 +182,150 @@ CREATE TABLE `transaksi` (
   `status_pengembalian` varchar(50) NOT NULL,
   `status_rental` varchar(50) NOT NULL,
   `bukti_pembayaran` varchar(120) NOT NULL,
-  `status_pembayaran` int(11) NOT NULL
+  `status_pembayaran` int(11) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `transaksi`
+-- Dumping data for table `transaksi`
 --
 
-INSERT INTO `transaksi` (`id_rental`, `id_customer`, `id_mobil`, `tgl_rental`, `tgl_kembali`, `harga`, `denda`, `total_denda`, `tgl_pengembalian`, `status_pengembalian`, `status_rental`, `bukti_pembayaran`, `status_pembayaran`) VALUES
-(3, 6, 9, '2021-01-15', '2021-01-16', '450000', 30000, '120000', '2021-01-20', 'Belum Kembali', 'Belum Selesai', '', 0),
-(4, 6, 6, '2021-01-15', '2021-01-18', '400000', 30000, '60000', '2021-01-20', 'Kembali', 'Selesai', 'tugas.png', 1),
-(5, 8, 10, '2021-01-18', '2021-01-19', '400000', 100000, '100000', '2021-01-20', 'Belum Kembali', 'Belum Selesai', '', 0),
-(6, 6, 11, '2021-01-28', '2021-01-30', '300000', 25000, '25000', '2021-01-31', 'Kembali', 'Selesai', 'tugas2.png', 1),
-(7, 9, 13, '2021-01-30', '2021-02-01', '350000', 25000, '25000', '2021-02-02', 'Kembali', 'Selesai', 'tugas2.png', 1),
-(8, 4, 14, '2021-09-17', '2021-09-19', '350000', 25000, '75000', '2021-09-22', 'Kembali', 'Selesai', '', 0);
+INSERT INTO `transaksi` (`id_rental`, `id_user`, `id_mobil`, `tgl_rental`, `tgl_kembali`, `harga`, `denda`, `total_denda`, `tgl_pengembalian`, `status_pengembalian`, `status_rental`, `bukti_pembayaran`, `status_pembayaran`, `created`, `updated`, `created_by`, `updated_by`) VALUES
+(3, 6, 9, '2021-01-15', '2021-01-16', '450000', 30000, '120000', '2021-01-20', 'Belum Kembali', 'Belum Selesai', '', 0, NULL, NULL, 0, 0),
+(4, 6, 6, '2021-01-15', '2021-01-18', '400000', 30000, '60000', '2021-01-20', 'Kembali', 'Selesai', 'tugas.png', 1, NULL, NULL, 0, 0),
+(5, 8, 10, '2021-01-18', '2021-01-19', '400000', 100000, '100000', '2021-01-20', 'Belum Kembali', 'Belum Selesai', '', 0, NULL, NULL, 0, 0),
+(6, 6, 11, '2021-01-28', '2021-01-30', '300000', 25000, '25000', '2021-01-31', 'Kembali', 'Selesai', 'tugas2.png', 1, NULL, NULL, 0, 0),
+(7, 9, 13, '2021-01-30', '2021-02-01', '350000', 25000, '25000', '2021-02-02', 'Kembali', 'Selesai', 'tugas2.png', 1, NULL, NULL, 0, 0),
+(8, 4, 14, '2021-09-17', '2021-09-19', '350000', 25000, '75000', '2021-09-22', 'Kembali', 'Selesai', '', 0, NULL, NULL, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(120) NOT NULL,
+  `username` varchar(120) NOT NULL,
+  `alamat` varchar(120) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `no_telepon` varchar(20) NOT NULL,
+  `no_ktp` varchar(50) NOT NULL,
+  `password` varchar(120) NOT NULL,
+  `role` int(11) NOT NULL COMMENT '1 = Admin, 2 = Customer',
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama`, `username`, `alamat`, `gender`, `no_telepon`, `no_ktp`, `password`, `role`, `created`, `updated`, `created_by`, `updated_by`) VALUES
+(4, 'Joko Santoso', 'joko', 'Jl. Satu Pekanbaru', 'laki-laki', '0653246512', '215654532767', '81dc9bdb52d04dc20036dbd8313ed055', 2, '2021-09-30 19:23:12', NULL, 0, 0),
+(5, 'Darmawan', 'darmawan', 'Jl. Dua Pekanbaru', 'laki-laki', '07617623', '1423477324723', '81dc9bdb52d04dc20036dbd8313ed055', 2, '2021-09-30 19:23:12', NULL, 0, 0),
+(6, 'Andi', 'andi', 'Jakarta', 'laki-laki', '0217687634', '12747657463', '827ccb0eea8a706c4c34a16891f84e7b', 2, '2021-09-30 19:23:12', NULL, 0, 0),
+(7, 'Arif', 'admin', 'Pekanbaru', 'laki-laki', '065423624', '1764578345', '21232f297a57a5a743894a0e4a801fc3', 1, '2021-09-30 19:23:12', NULL, 0, 0),
+(8, 'Bayu', 'bayu', 'Jl. Nangka Pekanbaru', 'laki-laki', '07612233', '14000756764735', '81dc9bdb52d04dc20036dbd8313ed055', 2, '2021-09-30 19:23:12', NULL, 0, 0),
+(9, 'Toni', 'toni', 'Bandung', 'laki-laki', '0835653243', '1753453265435', '81dc9bdb52d04dc20036dbd8313ed055', 2, '2021-09-30 19:23:12', NULL, 0, 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id_admin`);
-
---
--- Indeks untuk tabel `contact`
+-- Indexes for table `contact`
 --
 ALTER TABLE `contact`
   ADD PRIMARY KEY (`id_contact`);
 
 --
--- Indeks untuk tabel `customer`
+-- Indexes for table `fitur`
 --
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`id_customer`);
+ALTER TABLE `fitur`
+  ADD PRIMARY KEY (`id_fitur`);
 
 --
--- Indeks untuk tabel `mobil`
+-- Indexes for table `mobil`
 --
 ALTER TABLE `mobil`
   ADD PRIMARY KEY (`id_mobil`);
 
 --
--- Indeks untuk tabel `rental`
---
-ALTER TABLE `rental`
-  ADD PRIMARY KEY (`id_rental`);
-
---
--- Indeks untuk tabel `review`
+-- Indexes for table `review`
 --
 ALTER TABLE `review`
   ADD PRIMARY KEY (`id_review`);
 
 --
--- Indeks untuk tabel `tipe`
+-- Indexes for table `tipe`
 --
 ALTER TABLE `tipe`
   ADD PRIMARY KEY (`id_tipe`);
 
 --
--- Indeks untuk tabel `transaksi`
+-- Indexes for table `transaksi`
 --
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id_rental`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `contact`
+-- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
   MODIFY `id_contact` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `customer`
+-- AUTO_INCREMENT for table `fitur`
 --
-ALTER TABLE `customer`
-  MODIFY `id_customer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+ALTER TABLE `fitur`
+  MODIFY `id_fitur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT untuk tabel `mobil`
+-- AUTO_INCREMENT for table `mobil`
 --
 ALTER TABLE `mobil`
   MODIFY `id_mobil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT untuk tabel `rental`
---
-ALTER TABLE `rental`
-  MODIFY `id_rental` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `review`
+-- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
   MODIFY `id_review` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `tipe`
+-- AUTO_INCREMENT for table `tipe`
 --
 ALTER TABLE `tipe`
   MODIFY `id_tipe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `transaksi`
+-- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
   MODIFY `id_rental` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
