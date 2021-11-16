d049f3a6dc638fbaca31c2ee43d2f03302e989d7
irvanalfi <44670772+irvanalfi@users.noreply.github.com>
2021-11-16 07:27:00 +0700

    data table

--- application/views/admin/data_tipe.php
+++ application/views/admin/data_tipe.php
@@ -32,30 +32,42 @@
       </div>
     <?php endif ?>
 
-    <table class="table table-stripped table-bordered table-hover">
-      <thead>
-        <tr>
-          <th width="20px;">No</th>
-          <th>Nama Tipe</th>
-          <th>Kode Tipe</th>
-          <th width="180px;">Aksi</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php $no = 1;
-        foreach ($tipe as $tp) : ?>
-          <tr>
-            <td><?= $no++; ?></td>
-            <td><?= $tp['nama_tipe'] ?></td>
-            <td><?= $tp['kode_tipe'] ?></td>
-            <td>
-              <a href="<?= base_url('admin/data_tipe/update_tipe/') . $tp['id_tipe'] ?>" class="btn btn-sm btn-primary"><i class="fas fa-edit"></i></a>
-              <a href="<?= base_url('admin/data_tipe/delete_tipe/') . $tp['id_tipe'] ?>" class="btn btn-sm btn-danger" onclick="return confirm('Yakin ingin menghapus data ini?')"><i class="fas fa-trash"></i></a>
-            </td>
-          </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            <div class="table-responsive">
+              <table class="table table-striped" id="table-1">
+                <thead>
+                  <tr>
+                    <th width="20px">
+                      No.
+                    </th>
+                    <th>Nama Tipe</th>
+                    <th>Kode Tipe</th>
+                    <th width="180px;">Aksi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php $no = 1;
+                  foreach ($tipe as $tp) : ?>
+                    <tr>
+                      <td><?= $no++; ?></td>
+                      <td><?= $tp['kode_tipe'] ?></td>
+                      <td><?= $tp['nama_tipe'] ?></td>
+                      <td class="align-middle">
+                        <a href="<?= base_url('admin/data_tipe/update_tipe/') . $tp['id_tipe'] ?>" class="btn btn-sm btn-primary"><i class="fas fa-edit"></i></a>
+                        <a href="<?= base_url('admin/data_tipe/delete_tipe/') . $tp['id_tipe'] ?>" class="btn btn-sm btn-danger" onclick="return confirm('Yakin ingin menghapus data ini?')"><i class="fas fa-trash"></i></a>
+                      </td>
+                    </tr>
+                  <?php endforeach ?>
+                </tbody>
+              </table>
 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </div>
--- application/views/templateAdmin.php
+++ application/views/templateAdmin.php
@@ -10,9 +10,13 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="<?= base_url('node_modules/'); ?>datatables.net-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="<?= base_url('node_modules/'); ?>datatables.net-select-bs4/css/select.bootstrap4.min.css">
+
     <!-- Template CSS -->
-    <link rel="stylesheet" href="<?= base_url('assets/assets_stisla'); ?>/assets/css/style.css">
-    <link rel="stylesheet" href="<?= base_url('assets/assets_stisla'); ?>/assets/css/components.css">
+    <link rel="stylesheet" href="<?= base_url('assets/assets_stisla/'); ?>css/style.css">
+    <link rel="stylesheet" href="<?= base_url('assets/assets_stisla/'); ?>css/components.css">
 </head>
 
 <body>
@@ -58,7 +62,7 @@
             <?php echo $contents ?>
             <!-- End Content  -->
 
-            <footer class="main-footer fixed-bottom">
+            <footer class="main-footer">
                 <div class="footer-left">
                     <p>
                         Copyright &copy;
@@ -81,14 +85,23 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-    <script src="../assets/js/stisla.js"></script>
+    <script src="<?= base_url('assets/assets_stisla/'); ?>js/stisla.js"></script>
+
+    <!-- JS Libraies -->
+    <script src="<?= base_url('node_modules/'); ?>datatables/media/js/jquery.dataTables.min.js"></script>
+    <script src="<?= base_url('node_modules/'); ?>datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="<?= base_url('node_modules/'); ?>datatables.net-select-bs4/js/select.bootstrap4.min.js"></script>
+
 
     <!-- Template JS File -->
-    <script src="<?= base_url('assets/assets_stisla'); ?>/assets/js/scripts.js"></script>
-    <script src="<?= base_url('assets/assets_stisla'); ?>/assets/js/custom.js"></script>
+    <script src="<?= base_url('assets/assets_stisla/'); ?>js/scripts.js"></script>
+    <script src="<?= base_url('assets/assets_stisla/'); ?>js/custom.js"></script>
 
     <!-- Page Specific JS File -->
-    <script src="<?= base_url('assets/assets_stisla'); ?>/assets/js/page/index-0.js"></script>
+    <script src="<?= base_url('assets/assets_stisla/'); ?>js/page/modules-datatables.js"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="<?= base_url('assets/assets_stisla/'); ?>js/page/index-0.js"></script>
     <script type="text/javascript">
         $("#alert1").fadeTo(2000, 500).slideUp(500, function() {
             $("#alert1").slideUp(500);
